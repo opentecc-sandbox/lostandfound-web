709bf0d923f3d35a74ecfbc7ddc8f81b7fb831cf
hajar <hajarchoukaih@gmail.com>
2026-04-01 21:11:21 +0100

    add

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ||"https://lostandfound-api-tc60.onrender.com/api",
+    baseURL:"https://lostandfound-api-tc60.onrender.com/api",
     withCredentials: true
 
 })
