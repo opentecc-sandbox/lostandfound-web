8f7c5086e13e75af1fe3a80c559da8e3e92f4101
Hajar Choukaih <hajarchoukaih@gmail.com>
2026-04-02 14:24:42 +0100

    Setup GitHub Actions for Netlify Deployment (#3)

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios"
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
 
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "https://lostandfound-api-tc60.onrender.com/api",
+    withCredentials: true
 })
+
 export default api;
